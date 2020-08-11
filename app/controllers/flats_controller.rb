24cbcb78189ac7228a7db06bb9a1c5751ab4957b
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-08-11 17:54:13 +0100

    Rails add a lot of things including bootstrap and simple form

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,22 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating flats...'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Studio flat with excellent views',
+  address: '25 Cherry Lane, London N11 5NY',
+  description: 'Very light and airy, cosy in winter',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Basement vibes',
+  address: '113 Bristol road, London SE12 6HM',
+  description: 'Great for parties as the neighbours are very relaxed',
+  price_per_night: 150,
+  number_of_guests: 4
+)
+
+puts 'Finishing flats...'
