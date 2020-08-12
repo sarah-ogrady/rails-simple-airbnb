9599c3a488d5aced2b196dec7d593ffe8d4e41a9
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-08-12 10:52:21 +0100

    add crud actions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -8,15 +8,34 @@ class FlatsController < ApplicationController
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
   end
 
   def edit
+    @flat = Flat.find(params[:id])
   end
 
-  def delete
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
 
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :address, :price_per_night, :number_of_guests)
   end
 end
